7ae72c5655a03119677b13984535d57faf9fdff2
bparrish206 <brentparrish76@gmail.com>
2014-01-20 21:08:44 -0800

    Update http_server.py

--- assignments/session02/http_server.py
+++ assignments/session02/http_server.py
@@ -1,16 +1,35 @@
 import socket
 import sys
+import os
+import mimetypes
 
-
-def response_ok():
+def response_ok(body, mimetype):
     """returns a basic HTTP response"""
     resp = []
     resp.append("HTTP/1.1 200 OK")
-    resp.append("Content-Type: text/plain")
+    resp.append("Content-Type: {0}".format(mimetype))
     resp.append("")
-    resp.append("this is a pretty minimal response")
+    resp.append(body)
     return "\r\n".join(resp)
 
+def resolve_uri(uri):
+    base = 'webroot'
+    fileName = os.path.join(base, uri.lstrip('/'))
+    if os.path.isfile(filename):
+        mimetype = mimetypes.guess_type(fileName)[0]
+        body = open(filename, 'rb').read()
+        return body, mimetype
+    elif os.path.isdir(fileName):
+        bk = "\n".join(os.listdir(fileName))
+        return bk, 'text/ plain'
+    else:
+        return 'File Not Found'
+
+def response_not_found():
+    resp = []
+    resp.append("HTTP/1.1 404 Not Found")
+    resp.append("")
+    return "\r\n".join(resp)
 
 def response_method_not_allowed():
     """returns a 405 Method Not Allowed response"""
@@ -19,13 +38,13 @@ def response_method_not_allowed():
     resp.append("")
     return "\r\n".join(resp)
 
-
 def parse_request(request):
     first_line = request.split("\r\n", 1)[0]
     method, uri, protocol = first_line.split()
     if method != "GET":
         raise NotImplementedError("We only accept GET")
     print >>sys.stderr, 'request is okay'
+    return uri
 
 
 def server():
@@ -50,11 +69,17 @@ def server():
                         break
 
                 try:
-                    parse_request(request)
+                    uri = parse_request(request)
+                    body, mimetype = resolve_uri(uri)
+                    
                 except NotImplementedError:
                     response = response_method_not_allowed()
+
+                except ValueError:
+                    response = response_not_found()
+                    
                 else:
-                    response = response_ok()
+                    response = response_ok(body, mime)
 
                 print >>sys.stderr, 'sending response'
                 conn.sendall(response)
